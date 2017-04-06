a7e429eae621c6b2fc1c784e25a43d20138a9fb7
Adam Kelly <adam.leo.kelly@gmail.com>
2017-04-05 23:26:30 -0700

    Updated home page UI and removed circular progress
    Removed circular progress bar because the application did not flow
    correctly. Will replace it with horiziontal bars instead

--- app/components/home/homeCtrl.js
+++ app/components/home/homeCtrl.js
@@ -1,11 +1,22 @@
-var app = angular.module('Home', ['HomeService', 'chart.js', 'angular-svg-round-progressbar'])
+var app = angular.module('Home', ['HomeService', 'chart.js'])
 
 app.controller('homeCtrl', ['$scope', 'goalService', function($scope, goalService) {
   
 	$scope.goals  = [];
 	$scope.labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
+
+	var alterGoals = function(goals){
+		var incr = 0;
+		for(let goal of goals){
+			goal.completePercentage = incr;
+			incr += 25;
+			//console.log(goal);
+		}
+	}
+
 	goalService.getGoals().success(function(data){
+		alterGoals(data);
 		$scope.goals = data;
 		//console.log(data);
 	});
